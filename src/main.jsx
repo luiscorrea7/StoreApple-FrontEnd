9cc007c42e5577060e3fcad0612edf588b6fd9a0
Augusto Diaz <augustodiazgt@gmail.com>
2023-12-19 15:48:15 -0300

    error404-img-boton-volver

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FooterAD } from './components/footerPage/FooterAD.modules'
+import Error404 from './Pages/errorPage/Error404.module';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     {/* Elementos estaticos (Navbar, Footer, etc) afuera de Routes */}
       <Routes>
         {/* Diferentes vistas(Route) adentro de Routes*/}
+        <Route path='*' element={<Error404/>}/>
       </Routes>
       <FooterAD/>
     </BrowserRouter>
