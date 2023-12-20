e0a94cc9e017b7de5bfa54ca9c1ecf2a33ce8e8e
luis correa <ylss512c@gmail.com>
2023-12-20 04:11:33 -0300

    creando inputs e instalando react-hook-form

--- src/Pages/registerPage/RegisterPage.jsx
+++ src/Pages/registerPage/RegisterPage.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Col, Container, Row } from "react-bootstrap";
+import "../registerPage/RegisterPage.module.css";
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <form>
+      <Row className="justify-content-center g-0">
+        <Col xs={10} md={8} className="d-flex flex-column align-items-center">
+          <label htmlFor="userName">Nombre de usuario</label>
+          <input type="text" />
+        </Col>
+
+        <Col xs={10} md={8} className="d-flex flex-column align-items-center">
+          <label htmlFor="email">Correo</label>
+          <input type="email" />
+        </Col>
+
+        <Col xs={10} md={8} className="d-flex flex-column align-items-center">
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" />
+        </Col>
+
+        <Col xs={10} md={8} className="d-flex flex-column align-items-center">
+          <label htmlFor="repeatPassword">Repetir contraseña</label>
+          <input type="password" />
+        </Col>
+
+        <Col xs={10} md={8} className="d-flex flex-column align-items-center mt-3">
+          <input type="submit" />
+        </Col>
+
+        
+      </Row>
+    </form>
   )
 }
 
