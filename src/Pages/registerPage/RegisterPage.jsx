22b88681d1eda693f745d187986312f2ca44903c
luis correa <ylss512c@gmail.com>
2023-12-20 05:23:01 -0300

    validaciones register hechas

--- src/Pages/registerPage/RegisterPage.jsx
+++ src/Pages/registerPage/RegisterPage.jsx
@@ -1,28 +1,129 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import "../registerPage/RegisterPage.module.css";
 
 const RegisterPage = () => {
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: {errors}
+  } = useForm()
+
+  const submitForm = handleSubmit((data) => {
+    console.log('form data', data)
+  })
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <Row className="justify-content-center g-0">
+
         <Col xs={10} md={8} className="d-flex flex-column align-items-center">
           <label htmlFor="userName">Nombre de usuario</label>
-          <input type="text" />
+          <input type="text"
+            {... register("userName",
+            {
+              required: {
+                value: true,
+                message: "Este campo es requerido"
+              },
+              minLength: {
+                value: 4,
+                message: "Este campo requiere al menos 4 caracteres"
+              },
+              maxLength: {
+                value: 20,
+                message: "Este campo esta limitado a 20 caracteres"
+              },
+              pattern: {
+                value: /^[A-Za-z\s]+$/,
+                message: 'Este campo no puede recibir caracteres numericos',
+              },
+            })}
+          />
+          {
+            errors.userName && <span>{errors.userName.message}</span>
+          }
         </Col>
 
         <Col xs={10} md={8} className="d-flex flex-column align-items-center">
           <label htmlFor="email">Correo</label>
-          <input type="email" />
+          <input type="email"
+            {... register("email",
+            {
+              required: {
+                value: true,
+                message: "Este campo es requerido"
+              },
+              pattern: {
+                value: /^[\w\.=-]+@[\w\.-]+\.[\w]{2,3}$/,
+                message: "Ingrese un correo valido"
+              }
+            })}
+          />
+          {
+            errors.email && <span>{errors.email.message}</span>
+          }
         </Col>
 
         <Col xs={10} md={8} className="d-flex flex-column align-items-center">
           <label htmlFor="password">Contraseña</label>
-          <input type="password" />
+          <input type="password"
+            {... register("password",
+            {
+              required: {
+                value: true,
+                message: "Este campo es requerido"
+              },
+              pattern: {
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/,
+                message: "La contraseña no cumple los requisitos"
+              }
+            })}
+          />
+          {
+            errors.password && <span>{errors.password.message}</span>
+          }
+          <p>
+            La contraseña debe tener al menos.<br/>
+            Una letra mayuscula.<br/> 
+            Una letra minuscula.<br/>
+            Un caracter numerico.
+          </p>
         </Col>
 
         <Col xs={10} md={8} className="d-flex flex-column align-items-center">
           <label htmlFor="repeatPassword">Repetir contraseña</label>
-          <input type="password" />
+          <input type="password"
+            {... register("repeatPassword", 
+            {
+              required: {
+                value: true,
+                message: "Este campo es requerido"
+              },
+              validate: value => value === watch('password') || "Las contraseñas no coinciden"
+            })}
+          />
+          {
+            errors.repeatPassword && <span>{errors.repeatPassword.message}</span>
+          }
+        </Col>
+
+        <Col xs={10} md={8} className="d-flex flex-column align-items-center">
+          <label htmlFor="termsAndCons">Acepto terminos y condiciones</label>
+          <input type="checkbox"
+            {... register("termsAndCons",
+            {
+              required: {
+                value: true,
+                message: "Acepte los terminos y condiciones para continuar"
+              },
+            })}
+          />
+          {
+            errors.termsAndCons && <span>{errors.termsAndCons.message}</span>
+          }
         </Col>
 
         <Col xs={10} md={8} className="d-flex flex-column align-items-center mt-3">
@@ -31,6 +132,7 @@ const RegisterPage = () => {
 
         
       </Row>
+
     </form>
   )
 }
