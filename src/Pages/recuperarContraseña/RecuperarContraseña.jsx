b2a3e394f2fdeaf8da017d81363055ba5cb4c060
Augusto Diaz <augustodiazgt@gmail.com>
2023-12-21 20:22:34 -0300

    esqueleto formulario

---
+++ src/Pages/recuperarContraseña/RecuperarContraseña.jsx
@@ -0,0 +1,73 @@
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { Container, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import './Style.moduleee.css'
+
+    const RecuperarContraseña = () => {
+  const urlBackend = import.meta.env.VITE_BASE_URL;
+  const navigate =  useNavigate() 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = async(data, e) => {
+    e.preventDefault()
+    try {
+      const resp = await fetch(`${urlBackend}/login`, {
+        method:'POST',
+        body: JSON.stringify(data),
+        headers:{
+          "Content-Type": "application/json"
+        }
+      })
+      const json = await resp.json()
+      console.log(json);
+      Swal.fire({
+        title: 'Store',
+        text: json.mensaje,
+        icon: "success",
+        showConfirmButton: false,
+        timer: 2000
+      }).then(function() {
+        navigate('/')
+      })
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
+    return (
+        <Container className="d-flex justify-content-center mt-5 caja-login ">
+        <form onSubmit={handleSubmit(onSubmit)} className="col-lg-5 col-12">
+        <Row>
+        <h2 className="fw-bold text-center pt-2 mb-4 ">Ingrese su Correo</h2>
+          <div className="mb-1">
+            <label htmlFor="email" className="form-label">Correo</label><br />
+            <input 
+              required
+              type="email" 
+              name="email" 
+              className="form-control entrada-login" 
+              id="email" 
+              {...register("email", { 
+                required: "Debe ingresar un correo.",
+                pattern:  {
+                  value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "Ingresa un correo válido."
+                }
+                })
+              } 
+            />
+            {errors.email && <p className="error-icon">{errors.email.message}</p>}
+            <br />
+          </div>
+          <Col md={4} xs={6} lg={6} className="d-grid gap-2 mx-auto">
+            <button type="submit" className="btn boton-login" >Enviar</button>
+          </Col>
+        </Row>
+       </form>
+      </Container>
+        )
+  }
+  
+  export default RecuperarContraseña;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FooterAD } from './components/footerPage/FooterAD.modules'
 import Error404 from './Pages/errorPage/Error404.module';
+import RecuperarContraseña from './Pages/recuperarContraseña/RecuperarContraseña';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         {/* Diferentes vistas(Route) adentro de Routes*/}
         <Route path='*' element={<Error404/>}/>
+        <Route path='/RecCont' element={<RecuperarContraseña/>}/>
       </Routes>
       <FooterAD/>
     </BrowserRouter>
