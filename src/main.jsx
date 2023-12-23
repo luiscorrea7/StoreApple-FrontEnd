ef779041194732aa7859d21ed425ebedef38162f
Augusto Diaz <augustodiazgt@gmail.com>
2023-12-23 14:29:06 -0300

    estilos-del-register

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FooterAD } from './components/footerPage/FooterAD.modules'
 import Error404 from './Pages/errorPage/Error404.module';
 import RecuperarContraseña from './Pages/recuperarContraseña/RecuperarContraseña';
+import RegisterPage from './pages/registerPage/RegisterPage';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     {/* Elementos estaticos (Navbar, Footer, etc) afuera de Routes */}
       <Routes>
         {/* Diferentes vistas(Route) adentro de Routes*/}
+        <Route path='/register' element={<RegisterPage/>}/>
         <Route path='*' element={<Error404/>}/>
         <Route path='/RecCont' element={<RecuperarContraseña/>}/>
       </Routes>
