bcbdca5edcf3483365120b977f80bf07d219f7f2
AleGalarze21 <alegalarzeoficial@gmail.com>
2024-01-17 22:27:04 -0300

    Cambios NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,13 @@ import Error404 from './Pages/errorPage/Error404.module';
 import RegisterPage from './Pages/registerPage/RegisterPage';
 import RecuperarContraseña from './Pages/recupContraseña/RecuperarContraseña.module';
 import { HomePage } from './Pages/homePage/HomePage.module';
+import NavbarComp from './components/navbarComponent/NavbarComp.modules';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     {/* Elementos estaticos (Navbar, Footer, etc) afuera de Routes */}
+      <NavbarComp />
       <Routes>
         {/* Diferentes vistas(Route) adentro de Routes*/}
         <Route path="/home" element={<HomePage />} />
